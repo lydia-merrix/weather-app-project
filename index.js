d8dd11b8833c0d735f623e4bb32afc68741702cc
lydia merrix <lydiamerrix@gmail.com>
2021-11-06 17:42:22 -0400

    added row of temperature html and js

--- index.js
+++ index.js
@@ -26,6 +26,38 @@ let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", getCity);
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+<div class="col-2">
+  <div class="weather-forecast-date">${day}</div>
+  <img 
+  src="http://openweathermap.org/img/wn/10d@2x.png" 
+  alt="" 
+  width="42"
+  />
+  <div class="weather-forecast-temperatures">
+  <span class="weather-forecast-temperature-max" >
+18°
+</span> 
+<span class="weather-forecast-temperature-min" >
+  12°
+</span> 
+</div>
+</div>
+`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 // search engine
 //function search(event) {
 // event.preventDefault();
@@ -139,3 +171,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+displayForecast();
