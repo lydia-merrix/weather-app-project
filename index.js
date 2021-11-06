3deeaf0eb459ea1847ce02691cb559ee89da2e46
lydia merrix <lydiamerrix@gmail.com>
2021-11-05 20:23:35 -0400

    replaced brackets for time

--- index.js
+++ index.js
@@ -74,13 +74,13 @@ function formatTime(timestamp) {
   let time = new Date(timestamp * 1000);
   let hours = time.getHours();
   if (hours < 10) {
-    hours = `0$(hours)`;
+    hours = `0${hours}`;
   }
   let minutes = time.getMinutes();
   if (minutes < 10) {
-    minutes = `0$(minutes)`;
+    minutes = `0${minutes}`;
   }
-  return `$(hours):${minutes}`;
+  return `${hours}:${minutes}`;
 }
 
 function showTemperature(response) {
